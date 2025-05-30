930c2672c73676d957c3e6ed6d3b59d3e5f5c276
Dez56 <ezasitocarranza@gmail.com>
2025-05-29 22:32:22 -0600

    Clases agregadas

--- src/caballero.java
+++ src/caballero.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-
 public class caballero extends Char{
     public caballero(String name, boolean tea) {
         super(name, 5, 2, 2, 2, 3, tea, true);
